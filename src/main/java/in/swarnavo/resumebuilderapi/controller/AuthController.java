ec30f8938e7353bc8871e11ebd83aa6c5535a24b
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-08 18:20:09 +0530

    feat: file upload api endpoint added and tested

--- src/main/java/in/swarnavo/resumebuilderapi/controller/AuthController.java
+++ src/main/java/in/swarnavo/resumebuilderapi/controller/AuthController.java
@@ -3,6 +3,7 @@ package in.swarnavo.resumebuilderapi.controller;
 import in.swarnavo.resumebuilderapi.dto.AuthResponse;
 import in.swarnavo.resumebuilderapi.dto.RegisterRequest;
 import in.swarnavo.resumebuilderapi.service.AuthService;
+import in.swarnavo.resumebuilderapi.service.FileUploadService;
 import in.swarnavo.resumebuilderapi.util.AppConstants;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -20,6 +23,8 @@ import java.util.Map;
 public class AuthController {
 
     private final AuthService authService;
+
+    private final FileUploadService fileUploadService;
 
     @PostMapping(AppConstants.REGISTER)
     public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
@@ -34,5 +39,12 @@ public class AuthController {
         log.info("Inside AuthController - verifyEmail(): {}", token);
         authService.verifyEmail(token);
         return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "Email verified successfully!"));
+    }
+
+    @PostMapping(AppConstants.UPLOAD_IMAGE)
+    public ResponseEntity<?> uploadImage(@RequestPart("image")MultipartFile file) throws IOException {
+        log.info("Inside AuthController - uploadImage()");
+        Map<String, String> response =  fileUploadService.uploadSingleImage(file);
+        return ResponseEntity.ok(response);
     }
 }
