c380d26bc4df1c7876b002763cdd5c52813d0689
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-08 09:40:58 +0530

    feat: email finder method added in user repository

--- src/main/java/in/swarnavo/resumebuilderapi/repository/UserRepository.java
+++ src/main/java/in/swarnavo/resumebuilderapi/repository/UserRepository.java
@@ -3,5 +3,10 @@ package in.swarnavo.resumebuilderapi.repository;
 import in.swarnavo.resumebuilderapi.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
 }
