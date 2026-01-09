94c1639cb1a4297caf2f4ac3b00748c162bc021e
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-09 11:44:03 +0530

    feat: login request dto added

---
+++ src/main/java/in/swarnavo/resumebuilderapi/dto/LoginRequest.java
@@ -0,0 +1,23 @@
+package in.swarnavo.resumebuilderapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Name must be between 6 and 100 characters")
+    private String password;
+}
